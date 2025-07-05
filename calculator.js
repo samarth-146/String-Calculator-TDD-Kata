ff4b9f556260d07de14eb1300cf45f82275d820f
Samarth Ganjawala <samarthganjawala202@gmail.com>
2025-07-05 15:30:57 +0530

    Pass: return the exception with the numbers if negative number encounters

--- calculator.js
+++ calculator.js
@@ -2,19 +2,22 @@ const add = (numbers) => {
     if (numbers == "")
         return 0;
 
-    let delimeter=',';
-    if(numbers.startsWith("//"))
-    {
-        const [delimeters,number]=numbers.split('\n');
-        delimeter=delimeters.slice(2);
-        numbers=number;
+    let delimeter = ',';
+    if (numbers.startsWith("//")) {
+        const [delimeters, number] = numbers.split('\n');
+        delimeter = delimeters.slice(2);
+        numbers = number;
     }
 
-    return numbers
-        .replace(/\n/g, delimeter)
-        .split(delimeter)
-        .map(Number)
-        .reduce((sum, num) => sum + num, 0);
+    let array = numbers.replace(/\n/g, delimeter).split(delimeter).map(Number);
+
+    let negatives = array.filter((n) => {
+        return n < 0;
+    });
+    if (negatives.length > 0) {
+        throw new Error(`Negative numbers are not allowed ${negatives.join(',')}`);
+    }
+    return array.reduce((sum, num) => sum + num, 0);
 };
 
 module.exports = { add };
