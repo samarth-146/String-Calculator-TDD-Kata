0eb565fa737d394fc9247fd507f520cc536ed378
Samarth Ganjawala <samarthganjawala202@gmail.com>
2025-07-05 09:28:51 +0530

    Pass: return 0 for empty string

---
+++ calculator.js
@@ -0,0 +1,6 @@
+const add=(number)=>{
+    if(number=="")
+        return 0;
+};
+
+module.exports={add};
--- calculator.spec.js
+++ calculator.spec.js
@@ -1,3 +1,5 @@
+const {add}=require('./calculator');
+
 test('return 0 for empty string', () => {
   expect(add("")).toBe(0);
 });
