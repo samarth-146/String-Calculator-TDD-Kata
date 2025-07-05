78ffd97daa3ca683b5250c46d3e5b0722ed2cd9d
Samarth Ganjawala <samarthganjawala202@gmail.com>
2025-07-05 14:30:09 +0530

    Pass: custom delimeter support

--- calculator.js
+++ calculator.js
@@ -2,9 +2,17 @@ const add = (numbers) => {
     if (numbers == "")
         return 0;
 
+    let delimeter=',';
+    if(numbers.startsWith("//"))
+    {
+        const [delimeters,number]=numbers.split('\n');
+        delimeter=delimeters.slice(2);
+        numbers=number;
+    }
+
     return numbers
-        .replace(/\n/g, ',')
-        .split(',')
+        .replace(/\n/g, delimeter)
+        .split(delimeter)
         .map(Number)
         .reduce((sum, num) => sum + num, 0);
 };
